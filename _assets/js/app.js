6072067c15417b54b8d517700155940e16495320
h4llow3En <development@felixdoering.com>
2021-07-23 22:15:15 +0200

    Fix some litte menu things

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -3,12 +3,8 @@ $(function () {
   $('.mobile-toggle').on("click", function () {
     if ($('.navigation').hasClass('open-nav')) {
       $('.navigation').removeClass('open-nav');
-      $('.fas').removeClass('fa-times')
-      $('.fas').addClass('fa-bars')
     } else {
       $('.navigation').addClass('open-nav');
-      $('.fas').removeClass('fa-bars')
-      $('.fas').addClass('fa-times')
     }
   });
 });
