9f159bc3d41e970d8e6d6a2bc6c449c527a33618
h4llow3En <development@felixdoering.com>
2022-02-11 14:01:03 +0100

    Move sitemap gen to script

---
+++ script/sitemap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "<?xml version='1.0' encoding='UTF-8'?>" >sitemap.xml
+echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>" >>sitemap.xml
+for file in $(find . -type f -iname "*.html"); do
+  echo "<url><loc>https://felixdoering.com${file:1}</loc><lastmod>$(date -r $file)</lastmod></url>" >>sitemap.xml
+done
+echo "</urlset>" >>sitemap.xml
